77765f7c1441a1964b6f4c0f26ece6d6b709738a
Sahil Arora <sahil.ec@gmail.com>
2015-05-04 00:04:59 -0700

    Added some logic for Discover Bets.

--- app/src/main/java/com/salabs/betmo/Fragments/BetsDiscoverFragment.java
+++ app/src/main/java/com/salabs/betmo/Fragments/BetsDiscoverFragment.java
@@ -7,10 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.salabs.betmo.API.BetMoClient;
+import com.salabs.betmo.API.BetMoClientInterface;
+import com.salabs.betmo.Models.Bet;
 import com.salabs.betmo.R;
 
-public class BetsDiscoverFragment extends Fragment {
+import java.util.ArrayList;
 
+public class BetsDiscoverFragment extends Fragment implements BetMoClientInterface {
+
+    private ArrayList<Bet> bets;
 
     public static BetsDiscoverFragment newInstance() {
         BetsDiscoverFragment fragment = new BetsDiscoverFragment();
@@ -26,6 +32,9 @@ public class BetsDiscoverFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        bets = new ArrayList<Bet>();
+
         //if (getArguments() != null) {
         //}
     }
@@ -33,9 +42,15 @@ public class BetsDiscoverFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        bets.addAll(BetMoClient.getInstance(this).feedBets);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bets_discover, container, false);
     }
 
+    @Override
+    public void getAllBetsCallback(){
+    }
 
 }
